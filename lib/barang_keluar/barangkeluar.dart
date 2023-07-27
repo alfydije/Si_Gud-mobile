751dde8d2963a700ab0f423bcc5505dace252687
Cindyapriliyani <apriliyanicindy046@gmail.com>
2023-07-27 14:01:58 +0700

    cindy menambahkan form barang masuk dan keluar

--- lib/barang_keluar/barangkeluar.dart
+++ lib/barang_keluar/barangkeluar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:si_gud/barang_masuk/tambahbarangmasuk.dart';
 
 class BarangKeluarScreen extends StatelessWidget {
   @override
@@ -312,6 +313,95 @@ class BarangKeluarScreen extends StatelessWidget {
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Navigate to the TambahBarangScreen when the plus sign is clicked
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => TambahBarangScreen(
+                fotoBarang: '',
+                kodeBarang: '',
+                stokBarang: '',
+                namaBarang: '',
+                
+              ),
+            ),
+          );
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Color.fromARGB(255, 2, 31, 55),
+      ),
+    );
+  }
+
+  Widget buildItem(
+      String title, String subTitle, String stock, String imagePath) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 10),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 3,
+              blurRadius: 10,
+              offset: Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Row(
+          children: [
+            InkWell(
+              onTap: () {},
+              child: Container(
+                alignment: Alignment.center,
+                child: Image.asset(
+                  imagePath,
+                  height: 100,
+                  width: 80,
+                ),
+              ),
+            ),
+            Expanded( // Wrap the Row with Expanded to avoid overflow
+              child: Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      subTitle,
+                      style: TextStyle(
+                        fontSize: 14,
+                      ),
+                    ),
+                    Text(
+                      stock,
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
+// The TambahBarangScreen class remains unchanged as before
+
+ 
